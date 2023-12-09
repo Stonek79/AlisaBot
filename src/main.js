3df06856cbd41f5f53d3cd1e73c83db4275d1e57
Stonek79 <amit-yurist@mail.ru>
2023-12-09 18:58:28 +0300

    fix url

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import config from 'config'
 
 const bot = new Telegraf(config.get('TELEGRAM_SCHEDULE_TOKEN'), { handlerTimeout: 30000 })
 
-const url = 'https://ranepa-my.sharepoint.com/personal/sharapova-ev_ranepa_ru/_layouts/15/onedrive.aspx'
+const url = 'https://ion.ranepa.ru/students/study/'
 bot.start((ctx) => {
     const chatId = ctx.chat.id;
     ctx.replyWithHTML(`Привет, ${ctx.from.first_name}!`, {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: 'Посмотреть расписание ИОН: Широкий бакалавриат 1 курс', url }
+                    { text: 'Посмотреть расписание ИОН', url }
                 ]
             ]
         }
